1d91ad01b3a001cb8dadbd27c8b9f73b1656e8da
rob <rob@nospam.com>
2020-02-25 20:42:29 +0000

    sudo

--- 000-use-all-cores-makepkg-conf-v4.sh
+++ 000-use-all-cores-makepkg-conf-v4.sh
@@ -3,8 +3,8 @@
 # https://gist.github.com/soehub/fc07b86e2292c562328ee0dc2aadf740
 set -e
 
-sudo pacman -Syyu --noconfirm
-sudo pacman -S --noconfirm --needed sudo
+pacman -Syyu --noconfirm
+pacman -S --noconfirm --needed sudo
 
 numberofcores=$(grep -c ^processor /proc/cpuinfo)
 sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'$(($numberofcores+1))'"/g' /etc/makepkg.conf;
