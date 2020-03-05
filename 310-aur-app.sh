b2312e71dc2401daecc17c2c9897b8019fb3842c
rob <rob@nospam.com>
2020-03-05 09:44:07 +0000

    yay

--- 310-aur-app.sh
+++ 310-aur-app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+sudo pacman -Syyu --noconfirm
+
+pacman -S ca-certificates ca-certificates-cacert ca-certificates-mozilla ca-certificates-utils --noconfirm
+
 yay -S --noconfirm visual-studio-code-bin
 yay -S --noconfirm nordic-theme-git
 yay -S --noconfirm spotify
