72bdd4dd88ae2b920613ea2c71a68af7dd761e22
rob <rob@nospam.com>
2020-03-05 09:44:50 +0000

    yay

--- 300-install-yay.sh
+++ 300-install-yay.sh
@@ -3,6 +3,9 @@ set -e
 
 cd /tmp
 
+sudo pacman -Syyu --noconfirm
+pacman -S ca-certificates ca-certificates-cacert ca-certificates-mozilla ca-certificates-utils --noconfirm
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
--- 310-aur-app.sh
+++ 310-aur-app.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo pacman -Syyu --noconfirm
-
-pacman -S ca-certificates ca-certificates-cacert ca-certificates-mozilla ca-certificates-utils --noconfirm
 
 yay -S --noconfirm visual-studio-code-bin
 yay -S --noconfirm nordic-theme-git
